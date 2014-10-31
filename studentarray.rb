c3ce0e5a464d00c862230056f809e977c6360888
tophat8855 <hrfarley@gmail.com>
2014-10-30 18:45:32 -0700

    student array init

---
+++ studentarray.rb
@@ -0,0 +1,8 @@
+student_data = [{first_name: "John",      last_name: "Foley",       email: "john@gschool.it",       class: "Beginning snark"},
+                {first_name: "Sylwester", last_name: "Kelsey",      email: "sellie@gmail.com",      class: "Ruby Immersive"},
+                {first_name: "Timothy",   last_name: "Rama",        email: "tim.rama@gmail.com",    class: "Ruby Immersive"},
+                {first_name: "Kane",      last_name: "Baccigalupi", email: "kane@gschool.it",       class: "C for dummies"},
+                {first_name: "Nikita",    last_name: "Theodosius",  email: "nikita.theo@gmail.com", class: "Ruby Immersive"},
+                {first_name: "Roddy",     last_name: "Eldred",      email: "roddy.el@gmail.com",    class: "Ruby Immersive"},
+                {first_name: "Marha",     last_name: "Berner",      email: "martha@gschool.it",     class: "Time travel for beginners"},
+                {first_name: "Kofi",      last_name: "Thomas",      email: "k.thomas@hotmail.com",  class: "Ruby Immersive"}]
